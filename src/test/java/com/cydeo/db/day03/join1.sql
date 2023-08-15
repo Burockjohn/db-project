0c55f721a3a78b8754e12cb27a6da4828fbbae58
Burockjohn <brkcn87@gmail.com>
2023-08-15 16:47:09 +0300

    day 3 - DDL & DML, Joins

--- src/test/java/com/cydeo/db/day03/join1.sql
+++ src/test/java/com/cydeo/db/day03/join1.sql
@@ -4,6 +4,41 @@ from CUSTOMER;
 select *
 from ADDRESS;
 
+--inner join
 select FIRST_NAME, LAST_NAME, ADDRESS, PHONE
-from CUSTOMER c join ADDRESS a
+from CUSTOMER c
+         join ADDRESS a
               on c.ADDRESS_ID = a.ADDRESS_ID;
+
+--left (outer) join
+select FIRST_NAME, LAST_NAME, ADDRESS, PHONE
+from CUSTOMER c
+         left join ADDRESS a
+                   on c.ADDRESS_ID = a.ADDRESS_ID;
+
+--left (outer) join with where
+select FIRST_NAME, LAST_NAME, ADDRESS, PHONE
+from CUSTOMER c
+         left join ADDRESS a
+                   on c.ADDRESS_ID = a.ADDRESS_ID
+where c.ADDRESS_ID is null;
+
+--right (outer) join
+select FIRST_NAME, LAST_NAME, ADDRESS, PHONE
+from CUSTOMER c
+         right join ADDRESS a
+                    on c.ADDRESS_ID = a.ADDRESS_ID;
+
+--full (outer) join
+select FIRST_NAME, LAST_NAME, ADDRESS, PHONE
+from CUSTOMER c
+         full join ADDRESS a
+                   on c.ADDRESS_ID = a.ADDRESS_ID;
+
+--full (outer) join with where
+select FIRST_NAME, LAST_NAME, ADDRESS, PHONE
+from CUSTOMER c
+         full join ADDRESS a
+                   on c.ADDRESS_ID = a.ADDRESS_ID
+where c.ADDRESS_ID is null
+   or a.ADDRESS_ID is null;
---
+++ src/test/java/com/cydeo/db/day03/join2.sql
@@ -0,0 +1,10 @@
+select *
+from EMPLOYEES;
+
+select *
+from DEPARTMENTS;
+
+select FIRST_NAME, LAST_NAME, DEPARTMENT_NAME
+from EMPLOYEES e
+         left join DEPARTMENTS d
+              on e.DEPARTMENT_ID = d.DEPARTMENT_ID;
