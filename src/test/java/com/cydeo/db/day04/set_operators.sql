87d394c551eeefa3e0a9319a92f5d8510794b209
Burockjohn <brkcn87@gmail.com>
2023-08-21 15:05:56 +0300

    day 4 - join with where, self join, set operators

--- src/test/java/com/cydeo/db/day04/set_operators.sql
+++ src/test/java/com/cydeo/db/day04/set_operators.sql
@@ -1,14 +1,32 @@
-select NAMES from testers
+select NAMES
+from testers
 union
-select NAMES from DEVELOPERS;
+select NAMES
+from DEVELOPERS;
 
-select NAMES from testers
+select NAMES
+from testers
 union all
-select NAMES from DEVELOPERS;
+select NAMES
+from DEVELOPERS;
 
-select NAMES from testers
+select NAMES
+from testers
 minus
-select NAMES from DEVELOPERS;
+select NAMES
+from DEVELOPERS;
+
+select NAMES
+from testers
+intersect
+select NAMES
+from DEVELOPERS;
+
+-- How to find duplicate names in employees table
+select FIRST_NAME, count(*)
+from EMPLOYEES
+group by FIRST_NAME
+having count(*) > 1;
 
 
 
