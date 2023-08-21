254989c019f4dd7cea82260082a79eee27a1cbc2
Burockjohn <brkcn87@gmail.com>
2023-08-21 16:41:33 +0300

    day 4 - jdbc intro

---
+++ src/test/java/com/cydeo/jdbc_tests/day04/TestConnection.java
@@ -0,0 +1,32 @@
+package com.cydeo.jdbc_tests.day04;
+
+import java.sql.*;
+
+public class TestConnection {
+
+    public static void main(String[] args) throws SQLException {
+
+        String dbUrl = "jdbc:oracle:thin:@54.166.75.122:1521:xe";
+        String dbUsername = "hr";
+        String dbPassword = "hr";
+
+        Connection connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from regions");
+
+        // next() --> move pointer to first row
+        resultSet.next();
+
+        //getting information with column name
+        System.out.println(resultSet.getString("region_name"));
+        //getting information with column index(starts from 1)
+        System.out.println(resultSet.getString(2));
+
+        // close connection
+        resultSet.close();
+        statement.close();
+        connection.close();
+
+    }
+
+}
